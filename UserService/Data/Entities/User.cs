2e382956cfbf47d33ecdbdc65244d5e40b5fe192
Donivejsa <liridon.vejsa@hotmail.com>
2025-09-24 19:43:25 +0200

    Update User.cs

--- UserService/Data/Entities/User.cs
+++ UserService/Data/Entities/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
